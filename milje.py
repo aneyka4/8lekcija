54a1094083fd92cc3cb91a85e5964b8661251a9c
aneyka4 <ana.budimir4@gmail.com>
2017-04-09 10:44:39 +0200

    first commit

---
+++ milje.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+print("Živijo! S tem pregramom se pretvarjajo kilometri v milje!")
+
+while True:
+    num = int(raw_input("Vnesi število kilometrov: "))
+    km = str(num*0.62)
+    print("Odgovor je " + km + " milj.")
+    vpr = (raw_input("Če želite nadaljevati vtipkajte DA, če želite končati vtipkajte NE"))
+    if vpr == "da":
+        continue
+    if vpr == "ne":
+        print("Hvala in nasvidenje!")
+        break7
+        7
